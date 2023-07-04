ca5289a8f2b8bd5f100f79c79cd6579b762b1f6c
gk646 <gk646@proton.me>
2023-07-04 02:52:35 +0200

    refactoring of folder names mkdir cxutil

--- src/cxutil/cxtime.h
+++ src/cxutil/cxtime.h
@@ -25,7 +25,7 @@
 #include <iomanip>
 #include <iostream>
 #include <string>
-#include "cxconfig.h"
+#include "../cxconfig.h"
 
 namespace cxutil {
 using namespace std;  //std:: makes this code unreadable
